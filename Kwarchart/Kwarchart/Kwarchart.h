798122d3ab0698acdac860b8a9642f1cf12a8754
Jireh Cordova <amieljireh_cordova@ligph.com>
2021-03-28 23:37:32 +0800

    Initial framework project commit.

---
+++ Kwarchart/Kwarchart/Kwarchart.h
@@ -0,0 +1,18 @@
+//
+//  Kwarchart.h
+//  Kwarchart
+//
+//  Created by Amiel Jireh Cordova on 3/28/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Kwarchart.
+FOUNDATION_EXPORT double KwarchartVersionNumber;
+
+//! Project version string for Kwarchart.
+FOUNDATION_EXPORT const unsigned char KwarchartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kwarchart/PublicHeader.h>
+
+
