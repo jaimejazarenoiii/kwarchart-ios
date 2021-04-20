5b1e85ef5de1ba4a4febb77393f1a721fa96610b
Jireh Cordova <amieljireh_cordova@ligph.com>
2021-04-20 14:22:35 +0800

    Initial commit for the kwarchart-demo example app

---
+++ Kwarchart-Example/Kwarchart-Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kwarchart-Example
+//
+//  Created by Amiel Jireh Cordova on 4/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kwarchart-Example/Kwarchart-Example/Kwarchart_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kwarchart_ExampleApp.swift
+//  Kwarchart-Example
+//
+//  Created by Amiel Jireh Cordova on 4/20/21.
+//
+
+import SwiftUI
+
+@main
+struct Kwarchart_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
