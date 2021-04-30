d66e24525cb317d2f959a7071711954fd5567de8
Jireh Cordova <amieljireh_cordova@ligph.com>
2021-04-30 12:45:05 +0800

    Create pie chart base shape.

--- Kwarchart-Example/Kwarchart-Example/ContentView.swift
+++ Kwarchart-Example/Kwarchart-Example/ContentView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        Text("Hello, world!")
+//            .padding()
+        
+//        Path() { path in
+//            path.move(to: CGPoint(x: 20, y: 20))
+//            path.addLine(to: CGPoint(x: 300, y: 20))
+//            path.addLine(to: CGPoint(x: 300, y: 200))
+//            path.addLine(to: CGPoint(x: 20, y: 200))
+//            path.closeSubpath()
+//        }
+//        .stroke(Color.green, lineWidth: 10)
+        //.fill(Color.green)
+        
+//        Path() { path in
+//            path.move(to: CGPoint(x: 20, y: 60))
+//            path.addLine(to: CGPoint(x: 40, y: 60))
+//            path.addQuadCurve(to: CGPoint(x: 210, y: 60), control: CGPoint(x: 125, y: 0))
+//            path.addLine(to: CGPoint(x: 230, y: 60))
+//            path.addLine(to: CGPoint(x: 230, y: 100))
+//            path.addLine(to: CGPoint(x: 20, y: 100))
+//        }
+        
+        Path { path in
+            path.move(to: CGPoint(x: 200, y: 200))
+            path.addArc(center: .init(x: 200, y: 200 ), radius: 150, startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 270), clockwise: true)
+        }
+        .fill(Color.green)
     }
 }
 
