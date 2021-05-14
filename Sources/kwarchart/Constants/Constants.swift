b707fe723b21a40daedd230b9c17a9079868cb73
Jireh Cordova <amieljireh_cordova@ligph.com>
2021-05-14 16:36:06 +0800

    Create constants file and library entry point. Remove renamed swift file.
    - Update project file.

---
+++ Sources/kwarchart/Constants/Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  kwarchart
+//
+//  Created by Amiel Jireh Cordova on 5/10/21.
+//
+
+import Foundation
+
+enum ChartType {
+    case PieChart
+}
+
+enum PieChartType {
+    case NORMAL
+    case DOUGHNUT
+}
+
+enum BarGraphType {
+    
+}
+
+enum 
+
+enum LegendPosition {
+    case NON
+    case TOP
+    case BOTTOM
+    case LEFT
+    case RIGHT
+}
---
+++ Sources/kwarchart/KwarChart.swift
@@ -0,0 +1,23 @@
+//
+//  KwarChart.swift
+//  kwarchart
+//
+//  Created by Amiel Jireh Cordova on 5/10/21.
+//
+
+struct KwarChart {
+    
+    func createPieChartView (pieChartType: PieChartType, legendPosition: LegendPosition) {
+        switch pieChartType {
+        case .NORMAL:
+            // create
+            break;
+        case .DOUGHNUT:
+            // create doughnut piechart
+            break;
+        }
+    }
+    
+    func createBarGraph() {}
+    func createLineGraph() {}
+}
