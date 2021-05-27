5e64b66334d18068493fd2797aec59a4d54756e4
Jireh Cordova <amieljireh_cordova@ligph.com>
2021-05-27 14:38:02 +0800

    Add LegendPosition and PieChart type constants.

---
+++ Sources/kwarchart/enum/LegendPosition.swift
@@ -0,0 +1,19 @@
+//
+//  LegendPosition.swift
+//  kwarchart
+//
+//  Created by Amiel Jireh Cordova on 5/26/21.
+//
+
+import Foundation
+
+enum LegendPosition {
+    case LEFT
+    case TOP_LEFT
+    case TOP
+    case TOP_RIGHT
+    case RIGHT
+    case BOTTOM_RIGHT
+    case BOTTOM
+    case BOTTOM_LEFT
+}
---
+++ Sources/kwarchart/enum/PieChartType.swift
@@ -0,0 +1,13 @@
+//
+//  PieChartType.swift
+//  kwarchart
+//
+//  Created by Amiel Jireh Cordova on 5/26/21.
+//
+
+import Foundation
+
+enum PieChartType {
+    case NORMAL
+    case DOUGHNUT
+}
