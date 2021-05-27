d92eae6050dd3008d4f4ac0ef91c7a27a439a710
Jireh Cordova <amieljireh_cordova@ligph.com>
2021-05-27 14:39:45 +0800

    Add PieChart cell drawing and data classes.
    Add a view that implements the pie chart drawing classes.

---
+++ Sources/kwarchart/charts/PieChart/PieChartDataModel.swift
@@ -0,0 +1,34 @@
+//
+//  PieChartDataModel.swift
+//  kwarchart
+//
+//  Created by Amiel Jireh Cordova on 5/27/21.
+//
+
+import Foundation
+import SwiftUI
+
+final class PieChartDataModel: ObservableObject {
+    var chartCellModels: [PieSeries<Any>] = []
+    var startingAngle = Angle(degrees: 0)
+    private var finalRelativeAngle = Angle(degrees: 0)
+    
+    init(dataModel: [PieSeries<Any>]) {
+        chartCellModels = dataModel
+    }
+    
+    var totalValue: CGFloat {
+        chartCellModels.reduce(CGFloat(0)) { (result, data) -> CGFloat in
+            result + data.data.value
+        }
+    }
+    
+    func angle(for value: CGFloat) -> Angle {
+        if startingAngle != finalRelativeAngle {
+            startingAngle = finalRelativeAngle
+        }
+        finalRelativeAngle += Angle(degrees: Double(value / totalValue) * 360)
+        print(finalRelativeAngle.degrees)
+        return finalRelativeAngle
+    }
+}
---
+++ Sources/kwarchart/charts/PieChart/PieChartView.swift
@@ -0,0 +1,29 @@
+//
+//  PieChartView.swift
+//  kwarchart
+//
+//  Created by Amiel Jireh Cordova on 5/27/21.
+//
+
+import SwiftUI
+
+struct PieChartView: View {
+    @State private var selectedCell: UUID = UUID()
+    
+    var dataModel: PieChartDataModel
+    
+    var body: some View {
+        ZStack {
+            ForEach(dataModel.chartCellModels) { dataSet in
+                PieChartCell(startAngle: self.dataModel.angle(for: dataSet.data.value), endAngle: self.dataModel.startingAngle)
+                    .foregroundColor(dataSet.color)
+            }
+        }
+    }
+}
+
+struct PieChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        PieChartView(dataModel: PieChartDataModel.init(dataModel: ChartSample.pieSeriesSample))
+    }
+}
