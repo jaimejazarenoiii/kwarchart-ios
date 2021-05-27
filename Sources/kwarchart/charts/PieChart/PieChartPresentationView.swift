28502df4da4557f3226c86157783e5ab9a3da9d1
Jireh Cordova <amieljireh_cordova@ligph.com>
2021-05-27 14:44:24 +0800

    Add a presenter that arranges the piechart and the legends.
    This view will returned when calling KwarChart.createPieChartView()

---
+++ Sources/kwarchart/charts/PieChart/PieChartPresentationView.swift
@@ -0,0 +1,40 @@
+//
+//  PieChartPresentationView.swift
+//  kwarchart
+//
+//  Created by Amiel Jireh Cordova on 5/27/21.
+//
+
+import SwiftUI
+
+struct PieChartPresentationView: View {
+    
+    let chartView: PieChartView
+    let chartData: [PieSeries<Any>]
+    let legendPosition: LegendPosition
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            chartView
+                .aspectRatio(contentMode: .fit)
+            VStack(alignment: .leading) {
+                ForEach(chartData) { dataSet in
+                    HStack {
+                        Circle().foregroundColor(dataSet.color).fixedSize()
+                        Text(dataSet.legend).font(.footnote)
+                        Text("\(dataSet.data.value.description)").font(.footnote)
+                    }
+                }
+            }.aspectRatio(contentMode: .fit)
+            Spacer()
+        }
+    }
+}
+
+struct PieChartPresentationView_Previews: PreviewProvider {
+    static var previews: some View {
+        PieChartPresentationView(chartView: ChartSample.pieChartViewSample, chartData: ChartSample.pieSeriesSample, legendPosition: .RIGHT)
+    }
+}
+
