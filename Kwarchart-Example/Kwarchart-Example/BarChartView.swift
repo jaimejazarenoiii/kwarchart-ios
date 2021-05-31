ab102cdd2a673f5dcbd75133f7bed5b52f67bc74
Jireh Cordova <amieljireh_cordova@ligph.com>
2021-05-31 14:01:34 +0800

    Add views for pie, bar, and line chart

---
+++ Kwarchart-Example/Kwarchart-Example/BarChartView.swift
@@ -0,0 +1,22 @@
+//
+//  BarChartView.swift
+//  Kwarchart-Example
+//
+//  Created by Amiel Jireh Cordova on 5/31/21.
+//
+
+import SwiftUI
+
+struct BarChartView: View {
+    var body: some View {
+        VStack {
+            Text("Some bar chart")
+        }
+    }
+}
+
+struct BarChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarChartView()
+    }
+}
---
+++ Kwarchart-Example/Kwarchart-Example/LineChartView.swift
@@ -0,0 +1,22 @@
+//
+//  LineChartView.swift
+//  Kwarchart-Example
+//
+//  Created by Amiel Jireh Cordova on 5/31/21.
+//
+
+import SwiftUI
+
+struct LineChartView: View {
+    var body: some View {
+        VStack {
+            Text("Some line chart")
+        }
+    }
+}
+
+struct LineChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        LineChartView()
+    }
+}
