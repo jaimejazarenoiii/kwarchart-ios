6a1505df9f4e72d65bf514cac949dc31e97d8e0d
Jireh Cordova <amieljireh_cordova@ligph.com>
2021-06-02 17:49:56 +0800

    Use library to draw a basic pie chart on the sample app.

--- Kwarchart-Example/Kwarchart-Example/PieChartView.swift
+++ Kwarchart-Example/Kwarchart-Example/PieChartView.swift
@@ -6,11 +6,25 @@
 //
 
 import SwiftUI
+import kwarchart
 
 struct PieChartView: View {
+    let pieSeries = [PieSeries(data: ChartData(key: "Shopping", value: 500), color: .red, legend: "Shopping"),
+                    PieSeries(data: ChartData(key: "Food", value: 1000), color: .blue, legend: "Food"),
+                    PieSeries(data: ChartData(key: "Electric Bill", value: 500), color: .yellow, legend: "Electricity"),
+                    PieSeries(data: ChartData(key: "Car", value: 900), color: .green, legend: "Car"),
+                    PieSeries(data: ChartData(key: "Rent", value: 1000), color: .orange, legend: "Rent"),
+                    PieSeries(data: ChartData(key: "Insurance", value: 800), color: .black, legend: "Insurance")]
+
+    
     var body: some View {
         VStack(spacing: 10) {
+            KwarChart.drawPieChart(pieChartType: .NORMAL, pieSeries: pieSeries, legendPosition: .BOTTOM)
+                .frame(width: 300, height: 250, alignment: .center)
+                .padding()
+            
             Text("Some pie chart")
+            
             Text("Some doughnut chart")
         }
     }
