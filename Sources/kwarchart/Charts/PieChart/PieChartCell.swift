2448703f9827f9eb233439d6002d7df55177624b
Jireh Cordova <amieljireh_cordova@ligph.com>
2021-06-11 17:09:10 +0800

    Apply code styling changes.

--- Sources/kwarchart/Charts/PieChart/PieChartCell.swift
+++ Sources/kwarchart/Charts/PieChart/PieChartCell.swift
@@ -12,16 +12,16 @@ struct PieChartCell: Shape {
     let endAngle: Angle
     
     func path(in rect: CGRect) -> Path {
-        let center = CGPoint.init(x: (rect.origin.x + rect.width)/2, y: (rect.origin.y + rect.height)/2 )
+        let center = CGPoint.init(x: (rect.origin.x + rect.width) / 2, y: (rect.origin.y + rect.height) / 2)
         let radii = min(center.x, center.y)
         
-        let path = Path { p in
-            p.addArc(center: center,
+        let path = Path { cell in
+            cell.addArc(center: center,
                      radius: radii,
                      startAngle: startAngle,
                      endAngle: endAngle,
                      clockwise: true)
-            p.addLine(to: center)
+            cell.addLine(to: center)
         }
         
         return path
