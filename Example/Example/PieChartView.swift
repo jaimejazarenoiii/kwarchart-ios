438936b61b0eda7458f7b33af9fb4ea2f60a0ab0
Jireh Cordova <amieljireh_cordova@ligph.com>
2021-07-05 17:02:26 +0800

    Adjust inline alignment.

--- Example/Example/PieChartView.swift
+++ Example/Example/PieChartView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 import kwarchart
 
 struct PieChartView: View {
-    let pieSeries = [PieChartData(key: "Shopping", value: 500, color: .red, legend: "Shopping"),
-                     PieChartData(key: "Food", value: 1000, color: .blue, legend: "Food"),
-                     PieChartData(key: "Electric Bill", value: 500, color: .yellow, legend: "Electricity"),
-                     PieChartData(key: "Car", value: 900, color: .green, legend: "Car"),
-                     PieChartData(key: "Rent", value: 1000, color: .orange, legend: "Rent"),
-                     PieChartData(key: "Insurance", value: 800, color: .black, legend: "Insurance")]
+    let pieSeries = [ PieChartData(key: "Shopping", value: 500, color: .red, legend: "Shopping"),
+                      PieChartData(key: "Food", value: 1000, color: .blue, legend: "Food"),
+                      PieChartData(key: "Electric Bill", value: 500, color: .yellow, legend: "Electricity"),
+                      PieChartData(key: "Car", value: 900, color: .green, legend: "Car"),
+                      PieChartData(key: "Rent", value: 1000, color: .orange, legend: "Rent"),
+                      PieChartData(key: "Insurance", value: 800, color: .black, legend: "Insurance")]
     
     var body: some View {
         VStack(spacing: 10) {
