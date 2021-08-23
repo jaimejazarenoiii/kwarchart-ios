52f1c91fb03f8c8a716c92bee3e12f884565402b
Veverly Mae Ricaza <veverlymaericaza@Veverlys-MacBook-Pro.local>
2021-08-23 19:37:55 +0800

    Add bar graph view

---
+++ Sources/kwarchart/Bar Graph/BarGraphView.swift
@@ -0,0 +1,20 @@
+//
+//  BarGraphView.swift
+//  kwarchart
+//
+//  Created by Veverly Mae Ricaza on 8/23/21.
+//
+
+import SwiftUI
+
+struct BarGraphView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BarGraphView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarGraphView()
+    }
+}
