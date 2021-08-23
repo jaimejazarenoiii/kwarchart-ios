43b2186fbfb9612820361e89692fa3bd066a1499
Jireh Cordova <amieljireh_cordova@ligph.com>
2021-08-23 15:44:10 +0800

    Update example library implementation.

--- Example/Example/PieChartView.swift
+++ Example/Example/PieChartView.swift
@@ -9,17 +9,17 @@ import SwiftUI
 import kwarchart
 
 struct PieChartView: View {
-    let pieSeries = [ PieChartData(key: "Shopping", value: 500, color: .red, legend: "Shopping"),
-                      PieChartData(key: "Food", value: 1000, color: .blue, legend: "Food"),
-                      PieChartData(key: "Electric Bill", value: 500, color: .yellow, legend: "Electricity"),
-                      PieChartData(key: "Car", value: 900, color: .green, legend: "Car"),
-                      PieChartData(key: "Rent", value: 1000, color: .orange, legend: "Rent"),
-                      PieChartData(key: "Insurance", value: 800, color: .black, legend: "Insurance")]
+    let pieSeries = [  PieChartData(key: "Shopping", value: 100.00, color: .red, legend: "Shopping"),
+                      PieChartData(key: "Food", value: 100.00, color: .blue, legend: "Food"),
+                      PieChartData(key: "Electric Bill", value: 100.00, color: .yellow, legend: "Electricity"),
+                      PieChartData(key: "Car", value: 100.00, color: .green, legend: "Car"),
+                      PieChartData(key: "Rent", value: 100.00, color: .orange, legend: "Rent"),
+                      PieChartData(key: "Insurance", value: 100.00, color: .purple, legend: "Insurance")]
     
     var body: some View {
         VStack(spacing: 10) {
-            KCPieChartPresentationView(chartView: ChartSample.pieChartViewSample, chartData: PieChartDataModel(dataModel: pieSeries), legendPosition: LegendPosition.right)
-                .frame(width: 300, height: 250, alignment: .center)
+            KCPieChartPresentationView(pieChartType: PieChartType.normal, chartData: PieChartDataModel(dataModel: pieSeries), legendPosition: LegendPosition.right)
+                .frame(width: 350, height: 400, alignment: .center)
                 .padding()
             
             Text("Some pie chart")
